58eb7f3feb889a1a2b9b79ed654cd3ee627b0c81
olivermunkuli <olivermunkuli@outlook.com>
2023-01-24 12:42:18 +0200

    implement route guard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 import Home from './pages/Home';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import NewTicket from './pages/NewTicket';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
             <Route path='/' element={<Home />} />
             <Route path='/register' element={<Register />} />
             <Route path='/login' element={<Login />} />
+            <Route path='/new-ticket' element={<PrivateRoute />}>
+              <Route path='/new-ticket' element={<NewTicket />} />
+            </Route>
           </Routes>
         </div>
       </Router>
